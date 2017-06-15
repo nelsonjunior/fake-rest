99fb327771a5671bcf7745d225d521783f68ca86
nrsjnet <nrsjnet@gmail.com>
2017-06-15 20:27:27 -0300

    Alterações
    alteraçóes configuração porta

--- server.js
+++ server.js
@@ -117,11 +117,10 @@ app.get('/:recurso/', function(req, res) {
 
 });
 
-var server = app.listen(3000, function() {
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Example app listening at http://%s:%s", host, port)
+var server = app.listen(server_port, server_ip_address, function() {
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 
 })
